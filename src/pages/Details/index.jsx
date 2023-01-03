b16b96e3dfbe70b73f179ebf344a6e4d9169fab7
leandro <leandro.devmcz@gmail.com>
2023-01-03 11:40:16 -0300

    rocket-notes-vite

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export function Details(){ 
+
+  return(// um componente retorna um único elemento
+    <div>
+        <h1>Hello World!</h1>
+        <span>Leandro Mello</span>
+    </div>
+  )
+}; 
+   
