80285ffb592bbefa6b1a4a1a3f66e953b7c16a91
leandro <leandro.devmcz@gmail.com>
2023-01-04 10:44:00 -0300

    est-glob

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
 
@@ -12,6 +13,7 @@ import { Details } from './pages/Details';
 ReactDOM.createRoot(document.getElementById('root')).render( 
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </React.StrictMode>
