d7eed1c182768a01126c9d92340cc35ad777f5ab
leandro <leandro.devmcz@gmail.com>
2023-01-06 21:53:07 -0300

    details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,6 +5,7 @@ import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
 
 
+
 export function Details(){ 
 
   return(// um componente retorna um único elemento
