fbb2453fff6b8d13db3f80ebf87569e9585efb7c
leandro <leandro.devmcz@gmail.com>
2023-01-10 10:54:30 -0300

    foort

---
+++ src/components/Note/index.jsx
@@ -0,0 +1,22 @@
+import { Container } from './styles';
+
+export function Note({ data, ...rest}) {
+  return (
+    <Container {...rest}>
+      <h1>{data.title}</h1>
+
+      {
+        data.tags &&
+        <footer>
+          {
+            data.tags.map(tag => {
+              <span key={tag.id}>
+                {tag.name}
+              </span>
+            })
+          }
+        </footer>
+      }
+    </Container>
+  )
+}
