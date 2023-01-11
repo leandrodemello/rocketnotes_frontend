f9a8926f99549f8aa7e9120e556f8ded3e19e690
leandro <leandro.devmcz@gmail.com>
2023-01-11 09:55:56 -0300

    import

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,44 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Input } from '../../components/Imput';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from "./styles";
+
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="tetx"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button 
+          title="Entrar">
+        </Button>
+
+        <a href="#">
+          Criar conta
+        </a>
+        
+      </Form>
+
+      <Background/>
+      
+    </Container>
+  )
+}
