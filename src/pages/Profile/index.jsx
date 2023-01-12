6508c02db7a5b46a9e44c30f7993dd742ee61a81
leandro <leandro.devmcz@gmail.com>
2023-01-12 09:36:07 -0300

    profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,10 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Imput';
 import { Button } from '../../components/Button';
 
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 
 export function Profile() {
@@ -17,6 +17,23 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img 
+          src="https://github.com/leandrodemello.png" 
+          alt="Foto do usuário" 
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+
+        </Avatar>
+
         <Input 
           placeholder="Nome"
           type="text"
