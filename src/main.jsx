efda9bd4d9475778d4102a2adb29e8f2274ddf9c
leandro <leandro.devmcz@gmail.com>
2023-01-16 10:06:38 -0300

    app-auth

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { New } from './pages/New';
+import { Routes } from './routes';
 
 //ReactDOM permite navegar e manipular os elementos da página
 //document.getElementByid().render = Selecionar e pegar dentro do documento um elemento pelo id = "root" para renderizar um conteúdo.
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -32,6 +32,7 @@ export function SignIn() {
         </Button>
 
         <a href="#">
+          <FiLock />
           Criar conta
         </a>
         
