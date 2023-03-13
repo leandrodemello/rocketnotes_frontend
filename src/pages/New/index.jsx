aa73dc5b89920e046209fe475a40306f6dfbbd66
leandro <leandro.devmcz@gmail.com>
2023-03-13 10:15:42 -0300

    test

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -9,12 +9,22 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { Container, Form } from './styles';
 
+import { api } from '../../services/api';
+
+import { useNavigate } from 'react-router-dom';
+
 export function New(){
-  const  [links, setLinks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
-  const  [tags, setTags] = useState([]);
+  const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink]);
@@ -30,6 +40,21 @@ export function New(){
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
+
+  async function handleNewNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    });
+    alert("Nota criada com sucesso!");
+    navigate("/");
+  }
+
 
 
   return(
@@ -43,8 +68,15 @@ export function New(){
             <Link to="/">voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
-          <Textarea placeholder="Observações" />
+          <Input 
+            placeholder="Título" 
+            onChange={e => setTitle(e.target.value)}
+          />
+
+          <Textarea 
+            placeholder="Observações" 
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis" >
           {
@@ -73,7 +105,7 @@ export function New(){
                 <NoteItem 
                   key={String(index)}
                   value={tag}
-                  onClick={() => { }}  
+                  onClick={() => handleRemoveTag(tag)}  
                 />
               ))
             } 
@@ -89,7 +121,11 @@ export function New(){
             </div>
           </Section>
 
-          <Button title="Salvar"/>
+          <Button 
+            title="Salvar"
+            onClick={handleNewNote}
+
+          />
 
         </Form>
       </main>
