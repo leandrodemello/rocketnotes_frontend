0e52ac1791c76350822e919e6cbeb43bf16b8d01
leandro <leandro.devmcz@gmail.com>
2023-03-17 10:14:30 -0300

    att

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -85,7 +85,7 @@ export function Home(){
         <Input 
           placeholder="Pesquisar pelo título"
           icon={FiSearch} 
-          onChange={() => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
         />
 
       </Search>
